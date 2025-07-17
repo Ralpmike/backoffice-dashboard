17312b6f4d748ef12babab5f66fd7935890a86ea
Ralpmike <ralphmike996@gmail.com>
2025-07-17 16:00:41 +0100

    added padding to country-stats and text contrast

--- components/metrix.tsx
+++ components/metrix.tsx
@@ -8,7 +8,7 @@ export default function MetricsGrid() {
         <Card key={index} className="bg-primary border-transparent px-4 lg:p-6">
           <CardContent className="px-0">
             <div className="space-y-2 text-center md:text-left">
-              <p className="text-xl text-metrics">{metric.title}</p>
+              <p className="text-xl text-metrics font-medium">{metric.title}</p>
               <p className="text-2xl md:text-3xl lg:text-[2rem] xl:text-[41px] font-bold text-metrics-foreground leading-[3rem]">
                 {metric.value}
               </p>
